c3d87359775e364437becd912c6ac5ae854b0f1b
Matt McCormick <mbmccormick@outlook.com>
2013-05-12 18:22:29 -0700

    Updating packaging information and creating application icons.

--- HackerNews/Properties/AssemblyInfo.cs
+++ HackerNews/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HackerNews")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Matt McCormick  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
