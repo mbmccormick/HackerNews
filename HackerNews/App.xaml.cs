0690e1cf1d88efed44848dbc4a7339e0839aae25
Matt McCormick <matt@mbmccormick.com>
2013-05-16 14:12:34 -0700

    Adding code to prompt for Windows Phone Store reviews.

--- HackerNews/App.xaml.cs
+++ HackerNews/App.xaml.cs
@@ -11,6 +11,7 @@ using HackerNews.Common;
 using System.Collections;
 using System.Windows.Media;
 using HackerNews.API;
+using Microsoft.Phone.Tasks;
 
 namespace HackerNews
 {
@@ -78,6 +79,8 @@ namespace HackerNews
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
             SmartDispatcher.Initialize(RootFrame.Dispatcher);
+
+            this.PromptForMarketplaceReview();
         }
 
         private void Application_Activated(object sender, ActivatedEventArgs e)
@@ -119,6 +122,54 @@ namespace HackerNews
                 // An unhandled exception has occurred; break into the debugger
                 Debugger.Break();
             }
+        }
+
+        private void PromptForMarketplaceReview()
+        {
+            string currentVersion = IsolatedStorageHelper.GetObject<string>("CurrentVersion");
+            if (currentVersion == null)
+                currentVersion = App.VersionNumber;
+
+            DateTime installDate = IsolatedStorageHelper.GetObject<DateTime>("InstallDate");
+            if (installDate == DateTime.MinValue)
+                installDate = DateTime.UtcNow;
+
+            if (currentVersion != App.VersionNumber) // override if this is a new version
+                installDate = DateTime.UtcNow;
+
+            if (DateTime.UtcNow.AddDays(-3) >= installDate) // prompt after 3 days
+            {
+                CustomMessageBox messageBox = new CustomMessageBox()
+                {
+                    Caption = "Review Hacker News",
+                    Message = "It's been a few days since you downloaded Hacker News. Would you like to write a review for it in the Windows Phone Store?",
+                    LeftButtonContent = "yes",
+                    RightButtonContent = "no",
+                    IsFullScreen = false
+                };
+
+                messageBox.Dismissed += (s1, e1) =>
+                {
+                    switch (e1.Result)
+                    {
+                        case CustomMessageBoxResult.LeftButton:
+                            MarketplaceReviewTask marketplaceReviewTask = new MarketplaceReviewTask();
+                            marketplaceReviewTask.Show();
+
+                            installDate = DateTime.MaxValue; // they have rated, don't prompt again
+
+                            break;
+                        default:
+                            installDate = DateTime.UtcNow; // they did not rate, prompt again in 2 days
+                            break;
+                    }
+                };
+
+                messageBox.Show();
+            }
+
+            IsolatedStorageHelper.SaveObject<string>("CurrentVersion", App.VersionNumber); // save current version of application
+            IsolatedStorageHelper.SaveObject<DateTime>("InstallDate", installDate); // save install date
         }
 
         #region Phone application initialization
