0e42d45f21921eda72e1cd565d32cde06498366d
Semih Okur <semih.okur@gmail.com>
2013-09-07 00:25:06 -0500

    Replaced APM with TAP calls

--- HackerNews.API/ServiceClient.cs
+++ HackerNews.API/ServiceClient.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -33,180 +34,164 @@ namespace HackerNews.API
                 PostHistory = new List<string>();
         }
 
-        public void GetTopPosts(Action<List<Post>> callback)
+        public async Task GetTopPosts(Action<List<Post>> callback)
         {
             HttpWebRequest request = HttpWebRequest.Create("http://" + serverAddress + "/news") as HttpWebRequest;
             request.Accept = "application/json";
 
-            request.BeginGetResponse((result) =>
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            Stream stream = response.GetResponseStream();
+            UTF8Encoding encoding = new UTF8Encoding();
+            StreamReader sr = new StreamReader(stream, encoding);
+
+            JsonTextReader tr = new JsonTextReader(sr);
+            List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+
+            tr.Close();
+            sr.Dispose();
+
+            stream.Dispose();
+
+            foreach (var item in data)
             {
-                var response = request.EndGetResponse(result);
+                item.title = CleanText(item.title);
+                item.is_read = PostHistory.Contains(item.id);
 
-                Stream stream = response.GetResponseStream();
-                UTF8Encoding encoding = new UTF8Encoding();
-                StreamReader sr = new StreamReader(stream, encoding);
+                if (item.url.StartsWith("http") == false)
+                    item.url = "http://news.ycombinator.com/item?id=" + item.id;
+            }
 
-                JsonTextReader tr = new JsonTextReader(sr);
-                List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i] = FormatPost(data[i]);
+            }
 
-                tr.Close();
-                sr.Dispose();
-
-                stream.Dispose();
-
-                foreach (var item in data)
-                {
-                    item.title = CleanText(item.title);
-                    item.is_read = PostHistory.Contains(item.id);
-
-                    if (item.url.StartsWith("http") == false)
-                        item.url = "http://news.ycombinator.com/item?id=" + item.id;
-                }
-
-                for (int i = 0; i < data.Count; i++)
-                {
-                    data[i] = FormatPost(data[i]);
-                }
-
-                callback(data);
-
-            }, null);
+            callback(data);
         }
 
-        public void GetNewPosts(Action<List<Post>> callback)
+        public async Task GetNewPosts(Action<List<Post>> callback)
         {
             HttpWebRequest request = HttpWebRequest.Create("http://" + serverAddress + "/newest") as HttpWebRequest;
             request.Accept = "application/json";
 
-            request.BeginGetResponse((result) =>
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            Stream stream = response.GetResponseStream();
+            UTF8Encoding encoding = new UTF8Encoding();
+            StreamReader sr = new StreamReader(stream, encoding);
+
+            JsonTextReader tr = new JsonTextReader(sr);
+            List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+
+            tr.Close();
+            sr.Dispose();
+
+            stream.Dispose();
+
+            foreach (var item in data)
             {
-                var response = request.EndGetResponse(result);
+                item.title = CleanText(item.title);
+                item.is_read = PostHistory.Contains(item.id);
 
-                Stream stream = response.GetResponseStream();
-                UTF8Encoding encoding = new UTF8Encoding();
-                StreamReader sr = new StreamReader(stream, encoding);
+                if (item.url.StartsWith("http") == false)
+                    item.url = "http://news.ycombinator.com/item?id=" + item.id;
+            }
 
-                JsonTextReader tr = new JsonTextReader(sr);
-                List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i] = FormatPost(data[i]);
+            }
 
-                tr.Close();
-                sr.Dispose();
-
-                stream.Dispose();
-
-                foreach (var item in data)
-                {
-                    item.title = CleanText(item.title);
-                    item.is_read = PostHistory.Contains(item.id);
-
-                    if (item.url.StartsWith("http") == false)
-                        item.url = "http://news.ycombinator.com/item?id=" + item.id;
-                }
-
-                for (int i = 0; i < data.Count; i++)
-                {
-                    data[i] = FormatPost(data[i]);
-                }
-
-                callback(data);
-
-            }, null);
+            callback(data);
         }
 
-        public void GetAskPosts(Action<List<Post>> callback)
+        public async Task etAskPosts(Action<List<Post>> callback)
         {
             HttpWebRequest request = HttpWebRequest.Create("http://" + serverAddress + "/ask") as HttpWebRequest;
             request.Accept = "application/json";
 
-            request.BeginGetResponse((result) =>
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            Stream stream = response.GetResponseStream();
+            UTF8Encoding encoding = new UTF8Encoding();
+            StreamReader sr = new StreamReader(stream, encoding);
+
+            JsonTextReader tr = new JsonTextReader(sr);
+            List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+
+            tr.Close();
+            sr.Dispose();
+
+            stream.Dispose();
+
+            foreach (var item in data)
             {
-                var response = request.EndGetResponse(result);
+                item.title = CleanText(item.title);
+                item.is_read = PostHistory.Contains(item.id);
 
-                Stream stream = response.GetResponseStream();
-                UTF8Encoding encoding = new UTF8Encoding();
-                StreamReader sr = new StreamReader(stream, encoding);
+                if (item.url.StartsWith("http") == false)
+                    item.url = "http://news.ycombinator.com/item?id=" + item.id;
+            }
 
-                JsonTextReader tr = new JsonTextReader(sr);
-                List<Post> data = new JsonSerializer().Deserialize<List<Post>>(tr);
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i] = FormatPost(data[i]);
+            }
 
-                tr.Close();
-                sr.Dispose();
-
-                stream.Dispose();
-
-                foreach (var item in data)
-                {
-                    item.title = CleanText(item.title);
-                    item.is_read = PostHistory.Contains(item.id);
-
-                    if (item.url.StartsWith("http") == false)
-                        item.url = "http://news.ycombinator.com/item?id=" + item.id;
-                }
-
-                for (int i = 0; i < data.Count; i++)
-                {
-                    data[i] = FormatPost(data[i]);
-                }
-
-                callback(data);
-
-            }, null);
+            callback(data);
         }
 
-        public void GetComments(Action<CommentResponse> callback, string postId)
+        public async Task GetComments(Action<CommentResponse> callback, string postId)
         {
             HttpWebRequest request = HttpWebRequest.Create("http://" + serverAddress + "/item/" + postId) as HttpWebRequest;
             request.Accept = "application/json";
 
-            request.BeginGetResponse((result) =>
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            Stream stream = response.GetResponseStream();
+            UTF8Encoding encoding = new UTF8Encoding();
+            StreamReader sr = new StreamReader(stream, encoding);
+
+            JsonTextReader tr = new JsonTextReader(sr);
+            CommentResponse data = new JsonSerializer().Deserialize<CommentResponse>(tr);
+
+            tr.Close();
+            sr.Dispose();
+
+            stream.Dispose();
+
+            if (data.url.StartsWith("http") == false)
+                data.url = "http://news.ycombinator.com/item?id=" + data.id;
+
+            data = FormatCommentPost(data);
+
+            if (data.content != null)
             {
-                var response = request.EndGetResponse(result);
+                Comment child = new Comment();
 
-                Stream stream = response.GetResponseStream();
-                UTF8Encoding encoding = new UTF8Encoding();
-                StreamReader sr = new StreamReader(stream, encoding);
+                child.comments = null;
+                child.content = data.content;
+                child.id = null;
+                child.level = 0;
+                child.time_ago = data.time_ago;
+                child.title = data.user + " " + data.time_ago;
+                child.user = data.user;
 
-                JsonTextReader tr = new JsonTextReader(sr);
-                CommentResponse data = new JsonSerializer().Deserialize<CommentResponse>(tr);
+                data.comments.Insert(0, child);
+            }
 
-                tr.Close();
-                sr.Dispose();
+            int i = data.comments.Count - 1;
 
-                stream.Dispose();
+            while (i >= 0)
+            {
+                data.comments[i] = CleanCommentText(data.comments[i]);
+                data.comments[i] = FormatComment(data.comments[i]);
 
-                if (data.url.StartsWith("http") == false)
-                    data.url = "http://news.ycombinator.com/item?id=" + data.id;
+                i--;
+            }
 
-                data = FormatCommentPost(data);
-
-                if (data.content != null)
-                {
-                    Comment child = new Comment();
-
-                    child.comments = null;
-                    child.content = data.content;
-                    child.id = null;
-                    child.level = 0;
-                    child.time_ago = data.time_ago;
-                    child.title = data.user + " " + data.time_ago;
-                    child.user = data.user;
-
-                    data.comments.Insert(0, child);
-                }
-
-                int i = data.comments.Count - 1;
-
-                while (i >= 0)
-                {
-                    data.comments[i] = CleanCommentText(data.comments[i]);
-                    data.comments[i] = FormatComment(data.comments[i]);
-
-                    i--;
-                }
-
-                callback(data);
-
-            }, null);
+            callback(data);
         }
 
         public void MarkPostAsRead(string postId)
