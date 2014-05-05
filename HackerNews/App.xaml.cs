b90c4a1e4c7958a3b28880a99c089348e1f96ee9
Matt McCormick <matt@mbmccormick.com>
2014-05-05 14:34:50 -0700

    Removing unneeded code.

--- HackerNews/App.xaml.cs
+++ HackerNews/App.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows.Markup;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using HackerNews.Resources;
 using HackerNews.Common;
 using System.Collections;
 using System.Windows.Media;
