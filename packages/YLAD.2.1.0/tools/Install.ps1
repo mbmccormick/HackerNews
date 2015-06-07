8ad8e0279deccd5a4cf97fdef82a5d3084c86ee3
Matt McCormick <matt@mbmccormick.com>
2015-06-07 11:52:59 -0700

    Updating .gitignore and removing unwanted files.

--- packages/YLAD.2.1.0/tools/Install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$contentFolderItem = $project.ProjectItems.Item("Content")
-$aboutFolderItem = $contentFolderItem.ProjectItems.Item("About")
-$aboutStylesItem = $aboutFolderItem.ProjectItems.Item("AboutStyles.xaml")
-
-$aboutStylesItem.Properties.Item("BuildAction").Value = [int]2
